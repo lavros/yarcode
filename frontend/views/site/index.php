5da4a2c553a71f0ff2a8c9118cab2b751cb04f65
Yaroslav Chernousov <mxze0x@gmail.com>
2016-09-08 23:14:33 +0600

    Оформлен подвал сайта.

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,7 +2,7 @@
 /**
  * @var $this yii\web\View
  */
-$this->title = 'Наше скромное портфолио';
+$this->title = 'Web Studio';
 ?>
 
 <!-- Services Section -->
@@ -358,34 +358,6 @@ $this->title = 'Наше скромное портфолио';
         </div>
     </div>
 </section>
-
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <span class="copyright">Copyright &copy; Your Website 2016</span>
-            </div>
-            <div class="col-md-4">
-                <ul class="list-inline social-buttons">
-                    <li><a href="#"><i class="fa fa-twitter"></i></a>
-                    </li>
-                    <li><a href="#"><i class="fa fa-facebook"></i></a>
-                    </li>
-                    <li><a href="#"><i class="fa fa-linkedin"></i></a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <ul class="list-inline quicklinks">
-                    <li><a href="#">Privacy Policy</a>
-                    </li>
-                    <li><a href="#">Terms of Use</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</footer>
 
 <!-- Portfolio Modals -->
 <!-- Use the modals below to showcase details about your portfolio projects! -->
